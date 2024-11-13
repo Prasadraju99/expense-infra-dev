3bcdc85cba745ba32016d27fc02991e39c19970e
Prasadraju99 <evaraprasadraju.azure@gmail.com>
2024-11-13 23:10:34 +0530

    expense-infra-dev

--- 05-app-alb/data.tf
+++ 05-app-alb/data.tf
@@ -7,7 +7,7 @@ data "aws_ssm_parameter" "private_subnet_ids" {
   name = "/${var.project_name}/${var.environment}/private_subnet_ids"
 }
 
-# data "aws_ssm_parameter" app_alb_sg_id" {
-#   # /expense/dev/app_alb_sg_ids
-#   name = "/${var.project_name}/${var.environment}/app_alb_sg_id"
-# }
+data "aws_ssm_parameter" "app_alb_sg_id" {
+  # /expense/dev/app_alb_sg_ids
+  name = "/${var.project_name}/${var.environment}/app_alb_sg_id"
+}
